269e2e14ae0364b30d3b2a66cedd0f2105350b8c
Gustavo Felix <gustavo.santosfelix@gmail.com>
2024-07-30 12:32:19 -0300

    feat: creating structure to convert payload and save entity

--- src/main/java/br/com/cbyk/contas/domain/service/ContaService.java
+++ src/main/java/br/com/cbyk/contas/domain/service/ContaService.java
@@ -3,7 +3,12 @@ package br.com.cbyk.contas.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cbyk.contas.application.mapper.ContaMapper;
+import br.com.cbyk.contas.application.payload.ContaPayload;
+import br.com.cbyk.contas.application.response.ContaResponse;
+import br.com.cbyk.contas.domain.model.ContaEntity;
 import br.com.cbyk.contas.domain.repository.ContaRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ContaService {
@@ -13,7 +18,16 @@ public class ContaService {
 	@Autowired
 	public ContaService(final ContaRepository repository) {
 		this.repository = repository;
+	}
 
+	@Transactional
+	public ContaResponse cadastrarConta(final ContaPayload payload) {
+
+		ContaEntity entidade = ContaMapper.toEntity(payload);
+
+		ContaEntity contaSalva = repository.save(entidade);
+
+		return ContaMapper.toResponse(contaSalva);
 	}
 
 }
