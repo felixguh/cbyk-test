658727870c884a2293efeee0ee250ea099e75b14
Gustavo Felix <gustavo.santosfelix@gmail.com>
2024-07-30 12:51:11 -0300

    feat: creating exception when a conta is not found

---
+++ src/main/java/br/com/cbyk/contas/domain/exceptions/ContaNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package br.com.cbyk.contas.domain.exceptions;
+
+public class ContaNaoEncontradaException extends RuntimeException {
+
+	private static final long serialVersionUID = 7126593323070016439L;
+
+	public ContaNaoEncontradaException() {
+		super();
+	}
+
+}
