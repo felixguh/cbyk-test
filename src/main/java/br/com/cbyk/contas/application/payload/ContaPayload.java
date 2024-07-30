6096532cb706a2812090b0f859fc83f7b017d76a
Gustavo Felix <gustavo.santosfelix@gmail.com>
2024-07-30 13:43:48 -0300

    feat: adding annotation to validate if the values are null, empty etc

--- src/main/java/br/com/cbyk/contas/application/payload/ContaPayload.java
+++ src/main/java/br/com/cbyk/contas/application/payload/ContaPayload.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.annotation.Nonnull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,12 +19,16 @@ public class ContaPayload implements Serializable {
 
 	private static final long serialVersionUID = 1938273388333605582L;
 
+	@Nonnull
 	private LocalDate dataVencimento;
 
+	@Nonnull
 	private LocalDate dataPagamento;
 
+	@Nonnull
 	private BigDecimal valor;
 
+	@NotEmpty
 	private String descricao;
 
 }
