913a3fc91bd0289ead98ca4582cedd9b9491bc95
Gustavo Felix <gustavo.santosfelix@gmail.com>
2024-07-30 23:08:59 -0300

    feat: configure security

--- src/main/java/br/com/cbyk/contas/infrastructure/config/SecurityConfig.java
+++ src/main/java/br/com/cbyk/contas/infrastructure/config/SecurityConfig.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
 
 	@Value("${security.username}")
 	private String userName;
@@ -24,20 +24,20 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated()).httpBasic();
+		http.csrf().disable()
+				.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic()
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
 		return http.build();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
 	public UserDetailsService userDetailsService() {
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(
-				User.withUsername(userName).password(passwordEncoder().encode(password)).roles("USER").build());
-		return manager;
+		return new InMemoryUserDetailsManager(
+				User.withUsername(userName).password("{noop}" + password).roles("USER").build());
 	}
 }
