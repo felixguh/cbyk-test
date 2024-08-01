cc50f3e5e608d7b00ea1858e23076ad2f550ba1a
Gustavo Felix <gustavo.santosfelix@gmail.com>
2024-07-31 23:19:28 -0300

    feat: creating service to process csv and validate fields

---
+++ src/main/java/br/com/cbyk/contas/domain/service/CsvContaService.java
@@ -0,0 +1,138 @@
+package br.com.cbyk.contas.domain.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.cbyk.contas.application.advice.FieldErrorResponse;
+import br.com.cbyk.contas.application.payload.ContaCsvPayload;
+import br.com.cbyk.contas.application.response.ContaErrorResponse;
+import br.com.cbyk.contas.domain.enums.Campo;
+import br.com.cbyk.contas.domain.exceptions.QuantidadeDeCabecalhoNaoESuficienteException;
+import br.com.cbyk.contas.domain.exceptions.CabecalhosNaoSaoIguais;
+
+@Service
+public class CsvContaService {
+
+	private static final String HEADER = "Data Vencimento;Data Pagamento;Valor;Descricao";
+
+	private static final String SEPARATOR_LINE = ";";
+
+	private final ValidaCamposService validaCamposService;
+
+	public CsvContaService(final ValidaCamposService validaCamposService) {
+		this.validaCamposService = validaCamposService;
+	}
+
+	public List<ContaErrorResponse> receberArquivo(MultipartFile file) throws IOException {
+		List<ContaCsvPayload> list = proecessarArquivoEConverterParaObjeto(file);
+
+		List<ContaErrorResponse> campoErros = new ArrayList<>();
+
+		validarRegistros(list, campoErros);
+
+		if (!campoErros.isEmpty()) {
+			return campoErros;
+		}
+
+		return null;
+
+	}
+
+	private void validarRegistros(List<ContaCsvPayload> list, List<ContaErrorResponse> campoErros) {
+		Long index = 1L;
+
+		for (ContaCsvPayload item : list) {
+			List<FieldErrorResponse> errors = new ArrayList<>();
+
+			Campo.DATA_VENCIMENTO.validar(item.getDataVencimento(), errors, validaCamposService);
+			Campo.DATA_PAGAMENTO.validar(item.getDataPagamento(), errors, validaCamposService);
+			Campo.VALOR.validar(item.getValor(), errors, validaCamposService);
+			Campo.DESCRICAO.validar(item.getDescricao(), errors, validaCamposService);
+
+			if (!errors.isEmpty()) {
+				ContaErrorResponse errorResponse = ContaErrorResponse.builder().errors(errors).line(index).build();
+
+				campoErros.add(errorResponse);
+			}
+
+			index++;
+		}
+
+	}
+
+	private List<ContaCsvPayload> proecessarArquivoEConverterParaObjeto(MultipartFile file) throws IOException {
+		BufferedReader fileReader = new BufferedReader(
+				new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+
+		String currentLine;
+
+		List<ContaCsvPayload> sgatList = new ArrayList<>();
+
+		boolean verificarCabecalho = true;
+
+		while ((currentLine = fileReader.readLine()) != null) {
+			String[] linhas = currentLine.split(SEPARATOR_LINE);
+
+			if (verificarCabecalho) {
+				List<String> headers = Arrays.asList(linhas);
+
+				validarCabecalho(headers);
+
+				verificarCabecalho = false;
+
+				continue;
+			}
+
+			convertItemsToObject(linhas, sgatList);
+		}
+
+		return sgatList;
+	}
+
+	private void convertItemsToObject(String[] linhas, List<ContaCsvPayload> lista) {
+		if (linhas.length > 0) {
+
+			ContaCsvPayload response = ContaCsvPayload.builder()
+					.dataVencimento(Optional.ofNullable(linhas[0]).orElse(null))
+					.dataPagamento(Optional.ofNullable(linhas[1]).orElse(null))
+					.valor(Optional.ofNullable(linhas[2]).orElse(null))
+					.descricao(Optional.ofNullable(linhas[3]).orElse(null)).build();
+
+			lista.add(response);
+		}
+	}
+
+	private static void validarCabecalho(List<String> headers) {
+		validarQuantidadeDeCabecalhos(headers);
+		validarOrderDoCabecalho(headers);
+	}
+
+	private static void validarQuantidadeDeCabecalhos(List<String> cabecalhos) {
+		long quantidadeDeItens = cabecalhos.stream().count();
+
+		long quantidadeInformada = Arrays.asList(HEADER.split(SEPARATOR_LINE, -1)).stream().count();
+
+		if (quantidadeDeItens < quantidadeInformada) {
+			throw new QuantidadeDeCabecalhoNaoESuficienteException();
+		}
+	}
+
+	private static void validarOrderDoCabecalho(List<String> cabecalhos) {
+		String result = cabecalhos.stream().collect(Collectors.joining(SEPARATOR_LINE));
+
+		if (!result.equals(HEADER)) {
+			throw new CabecalhosNaoSaoIguais();
+		}
+	}
+
+}
